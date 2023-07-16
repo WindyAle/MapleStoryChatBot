63b58c485b0d8a4192d397963861a4caa87f835c
WindyAle <132745170+WindyAle@users.noreply.github.com>
2023-07-16 19:17:21 +0900

    Add directories via upload
    msgbot directory upload

---
+++ msgbot/Bots/잡담기능/잡담기능.js
@@ -0,0 +1,49 @@
+const scriptName="잡담기능.js";
+const room_name=["메이플"];
+
+const answer = ['네 주인님',
+                '나 여기 있담',
+                '왜 부르냠',
+                '싫은뎀',
+                '나는 돌의 정령이담',
+                'Zzz......',
+                '쉿 재획 중이담',
+                '귀찮게 하지마람',
+                '크르릉',
+                '메이플스토리 서비스 종료 소식을 전하게 되어 마음이 무겁습니다.'];
+                
+const quotes = ['아직도 이 겜을 하냠?',
+                '허접ㅋㅋ',
+                '메이플 하는 사람은 다 흑우담',
+                '너가 제일 못생겼담',
+                '이럴 시간에 재획이나 해람',
+                '어떻게 닉네임이 웜웜윔웜윔웜',
+                '버려진 야영지를 누가 버야지라고 불러'];
+                
+const menu = ['백반', '라면', '파스타', '치킨', '피자',
+              '돈까스', '파워 엘릭서', '닭가슴살 샐러드(이런걸 누가먹음?)', '재획비',
+              '스시', '라멘', '덮밥', '중국집', '부리또',
+              '국밥', '맥날', '롯데리아', '떡볶이', '냉면',
+              '설렁탕', '부대찌개', '백채', '연래춘', '도담',
+              '학식', '그냥 굶어람.', '뻥튀기', '물', '닭갈비',
+              '삼겹살', '교수님과 점심식사', '볶음밥', '오므라이스',
+              '카레', '아영이네', '제육', '삼각김밥', '백종원도시락',
+              '페이몬', '추천해주는대로 먹어람.', '추천해줘도 안먹잖아'];
+
+function response(room, msg, sender, isGroupChat, replier, ImageDB, packageName, threadId){
+   if(/*room_name.indexOf(room)!=-1 &&*/ msg.indexOf('/밥') != -1){
+      random_Menu = menu[Math.floor(Math.random()*menu.length)];
+      replier.reply(random_Menu);
+      return;
+   }   
+   else if(/*room_name.indexOf(room)!= -1 &&*/ msg.indexOf('돌정아') != -1){
+      random_Answer = answer[Math.floor(Math.random()*9)];
+      replier.reply(random_Answer);
+      return;
+   }
+   else if(/*room_name.indexOf(room)!=-1 &&*/ msg.indexOf('/소신발언') != -1){
+      random_Quotes = quotes[Math.floor(Math.random()*7)];
+      replier.reply(random_Quotes);
+      return;
+   }  
+}
---
+++ msgbot/Bots/초성퀴즈/초성퀴즈.js
@@ -0,0 +1,82 @@
+const scriptName = "초성퀴즈";
+
+//var url = org.jsoup.Jsoup.connect("https://junlog296373230.wordpress.com/2022/10/17/메이플-초성-db/").get();
+var url = org.jsoup.Jsoup.connect("https://windyale.github.io/MapleDB").get();
+url = String(url);
+
+var answerQuiz = [];
+var category = [];
+var hint = [];
+
+const topicRange = [0, 0, 0, 931, 698, 1578, 384];
+
+function response(room, msg, sender, isGroupChat, replier, imageDB, packageName) {
+   if(msg.indexOf('/퀴즈') != -1){            
+      var printQuiz = [];
+      var pick = 0;
+      var topic = 4;
+      
+      if(msg.length > 3){
+        topic = Number(msg.split(' ')[1]);
+            
+        if(topic != 1 && topic != 2 && topic != 3 && topic != 4){
+          replier.reply('나를 화나게 하지마람...');
+          return;
+        }
+        
+        pick = Math.floor(Math.random()*topicRange[topic+2]) + topicRange[topic+1] + topicRange[topic] + topicRange[topic-1] + 1;
+      }
+      else      
+        pick = Math.floor(Math.random()*3591) + 1;
+          
+      answerQuiz = url.split('이름: ')[pick].split(' 초성')[0];
+      printQuiz = url.split('초성: ')[pick].split(' 범주')[0];     
+      category  = url.split('범주: ')[pick].split(' 힌트')[0];
+      hint = url.split('힌트: ')[pick].split('\n')[0];
+      
+      if(category == 'NPC')
+        replier.reply('[' + hint + ' ' + category + ']\n' + printQuiz);
+      else
+        replier.reply('[' + category + ']\n' + printQuiz);
+        
+      return;
+   }
+   //답 입력
+   if(msg.indexOf('!') == 0){
+      var getAnswer = msg.split('!')[1];
+      
+      if(answerQuiz.length < 1)
+         return;
+      
+      if(getAnswer.indexOf(answerQuiz) != -1){
+         replier.reply(sender.slice(-3) + ' 정답이담!\n답은 \'' + answerQuiz + '\'이담!');
+         answerQuiz = [];
+         return;
+      }        
+      else
+         replier.reply('틀렸담... 메알못은 나가람.');   
+         return;
+   }
+   //힌트
+   if(msg == '/힌트'){
+      if(answerQuiz.length < 1)
+         return;
+      
+      if(category == 'NPC')
+         replier.reply('첫글자 힌트: ' + answerQuiz[0]);
+      else
+         replier.reply('힌트: ' + hint);  
+      return;
+   }
+   //정답
+   if(msg == '/정답'){
+      if(answerQuiz.length < 1)
+         return;
+       
+      replier.reply('답은 ' + answerQuiz + '이담.\n'
+                    + hint + '의 ' + category + '이담.\n\n'
+                    + '으휴 메알못들이담!');
+      answerQuiz = [];
+      return;
+   }
+}
